29b9afca58f468c6f034084b15aa788eaca1ec37
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-01-15 18:01:46 +0530

    Update video_renamer.py

--- helpers/video_renamer.py
+++ helpers/video_renamer.py
@@ -21,7 +21,7 @@ async def rnv2(bot , u):
     file_path = None
     
     if not u.reply_to_message:
-        await u.reply_text(text=f"Please Reply To Your Video !\n\nExample:\n**/rnv | filename**\n\nsee /help.", quote=True)
+        await u.reply_text(text=f"𝐏𝐥𝐞𝐚𝐬𝐞 𝐫𝐞𝐩𝐥𝐲 𝐭𝐨 𝐚 𝐯𝐢𝐝𝐞𝐨 𝐟𝐢𝐥𝐞!\n\n𝐄𝐱𝐚𝐦𝐩𝐥𝐞:\n**/rnv | 𝐅𝐢𝐥𝐞𝐧𝐚𝐦𝐞**\n\nsee /help.", quote=True)
         return
     
     logger.info(f"status: {status}")
@@ -35,7 +35,7 @@ async def rnv2(bot , u):
         ft = m.document or m.video
         fsize = get_size(ft.file_size)
     else:
-        await m.reply_text(text=f"Please Reply To Your Video !\n\nExample:\n**/rnv | filename**\n\nsee /help.", quote=True)
+        await m.reply_text(text=f"𝐏𝐥𝐞𝐚𝐬𝐞 𝐫𝐞𝐩𝐥𝐲 𝐭𝐨 𝐚 𝐯𝐢𝐝𝐞𝐨 𝐟𝐢𝐥𝐞!\n\n𝐄𝐱𝐚𝐦𝐩𝐥𝐞:\n**/rnv | 𝐅𝐢𝐥𝐞𝐧𝐚𝐦𝐞**\n\nsee /help.", quote=True)
         logger.info(f"No Video File !")
         return
     
@@ -74,7 +74,7 @@ async def rnv2(bot , u):
     #########################
     args = u.text.split("|")
     if (len(args) <= 1) or (len(args) > 2):
-        await m.reply_text(text=f"are you kidding me ?\n\nExample:\n`/rnv | filename`\n\nsee /hlep.", quote=True)
+        await m.reply_text(text=f"𝐖𝐡𝐚𝐭 𝐭𝐡𝐞 𝐡𝐞𝐥𝐥?\n\nExample:\n`/rnv | filename`\n\nsee /hlep.", quote=True)
         return
     #########################
     
@@ -85,7 +85,7 @@ async def rnv2(bot , u):
         if os.path.splitext(newname)[1] in video_types:
             pass
         else:
-            await m.reply_text(text=f"use video extension in filename !\n\nExample:**/rnv | filename**\n\nsee /help.", quote=True)
+            await m.reply_text(text=f"𝐔𝐬𝐞 𝐯𝐢𝐝𝐞𝐨 𝐞𝐱𝐭𝐞𝐧𝐬𝐢𝐨𝐧 𝐢𝐧 𝐟𝐢𝐥𝐞 𝐧𝐚𝐦𝐞!\n\n𝐄𝐱𝐚𝐦𝐩𝐥𝐞:**/rnv | 𝐅𝐢𝐥𝐞𝐧𝐚𝐦𝐞**\n\nsee /help.", quote=True)
             fsw = "app"
             return
     else:
@@ -95,7 +95,7 @@ async def rnv2(bot , u):
     #################################################################### Downloading Video
     status = True
     logger.info(f"status: {status}")
-    msg = await m.reply_text(text=f"⬇️ Trying To Download Video", quote=True)
+    msg = await m.reply_text(text=f"⏳𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐢𝐧𝐠...", quote=True)
     
     c_time = time.time()
     file_path = await bot.download_media(
@@ -103,18 +103,18 @@ async def rnv2(bot , u):
         file_name=oldname,
         progress=progress_for_pyrogram,
         progress_args=(
-            "⬇️ Downloading Video:",
+            "𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐢𝐧𝐠...:",
             msg,
             c_time
         )
     )
     if not file_path:
-        await msg.edit(f"⬇️ Downloading Video Failed !")
+        await msg.edit(f"𝐅𝐚𝐢𝐥𝐞𝐝 𝐭𝐨 𝐝𝐨𝐰𝐧𝐥𝐨𝐚𝐝!")
         status = False
         logger.info(f"status: {status}")
         await clean_up(file_path)
     else:
-        await msg.edit(f"🌄 Generating thumbnail ...")
+        await msg.edit(f"🌄 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐢𝐧𝐠 𝐭𝐡𝐮𝐦𝐛𝐧𝐚𝐢𝐥...")
         """
         probe = await stream_creator(file_path)
         video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
@@ -126,7 +126,7 @@ async def rnv2(bot , u):
         
         thumbnail, duration, width, height = await thumb_creator(file_path)
         
-        await msg.edit(f"⬆️ Trying to Upload as Video ...")
+        await msg.edit(f"𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠 𝐯𝐢𝐝𝐞𝐨 ...")
         c_time = time.time()
         try:
             
@@ -142,7 +142,7 @@ async def rnv2(bot , u):
                 reply_to_message_id=m.message_id,
                 progress=progress_for_pyrogram,
                 progress_args=(
-                    "⬆️ Uploading as Video:",
+                    "𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠 𝐕𝐢𝐝𝐞𝐨:",
                     msg,
                     c_time
                 )
@@ -152,7 +152,7 @@ async def rnv2(bot , u):
             logger.info(f"status: {status}")
             await clean_up(file_path)
         except Exception as e:
-            await msg.edit(f"❌ Uploading as Video Failed **Error:**\n\n{e}")
+            await msg.edit(f"❌ 𝐅𝐚𝐢𝐥𝐞𝐝 𝐭𝐨 𝐔𝐩𝐥𝐨𝐚𝐝 **𝐄𝐫𝐫𝐨𝐫:**\n\n{e}")
             status = False
             logger.info(f"status: {status}")
             await clean_up(file_path)
