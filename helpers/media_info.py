3f754f4b0498a5f8a4fb50b72a0019d5b55959d6
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-01-15 19:12:31 +0530

    Update media_info.py

--- helpers/media_info.py
+++ helpers/media_info.py
@@ -24,8 +24,8 @@ async def cinfo2(bot , m):
       else:
          fn = "𝐅𝐢𝐥𝐞 𝐧𝐚𝐦𝐞 𝐧𝐨𝐭 𝐝𝐞𝐭𝐞𝐜𝐭𝐞𝐝!"
       if m.document:
-         #await m.reply_text(text=f"📋 𝐋𝐢𝐧𝐤 𝐢𝐧𝐟𝐨:\n\n𝐅𝐢𝐥𝐞: `{cfname}`\nMime-Type: `{mt}`\n𝐒𝐢𝐳𝐞: `{url_size}`\n\n𝐔𝐬𝐞 /upload as reply to your link, it will upload your link to telegram.\n\nSee /help.", quote=True)
-         await m.reply_text(text=f"📋 𝐌𝐞𝐝𝐢𝐚 𝐢𝐧𝐟𝐨:\n\n𝐅𝐢𝐥𝐞: `{fn}`\nMime-Type: {ft.mime_type}\n𝐒𝐢𝐳𝐞: `{fsize}`\n\n𝐔𝐬𝐞 /rna to rename and edit audio tags.\n\nSee /help.", quote=True)
+         #await m.reply_text(text=f"📋 𝐋𝐢𝐧𝐤 𝐢𝐧𝐟𝐨:\n\n𝐅𝐢𝐥𝐞: `{cfname}`\nMime-Type: `{mt}`\n𝐒𝐢𝐳𝐞: `{url_size}`\n\n𝐔𝐬𝐞 /upload 𝐚𝐬 𝐫𝐞𝐩𝐥𝐲 𝐭𝐨 𝐲𝐨𝐮𝐫 𝐥𝐢𝐧𝐤, 𝐭𝐨 𝐮𝐩𝐥𝐨𝐚𝐝 𝐲𝐨𝐮𝐫 𝐥𝐢𝐧𝐤 𝐭𝐨 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦.\n\n𝐒𝐞𝐞 /help.", quote=True)
+         await m.reply_text(text=f"📋 𝐌𝐞𝐝𝐢𝐚 𝐢𝐧𝐟𝐨:\n\n𝐅𝐢𝐥𝐞: `{fn}`\nMime-Type: {ft.mime_type}\n𝐒𝐢𝐳𝐞: `{fsize}`\n\n𝐔𝐬𝐞 /rna 𝐭𝐨 𝐫𝐞𝐧𝐚𝐦𝐞 𝐚𝐧𝐝 𝐞𝐝𝐢𝐭 𝐚𝐮𝐝𝐢𝐨 𝐭𝐚𝐠𝐬.\n\n𝐒𝐞𝐞 /help.", quote=True)
          return
       if m.audio.title:
          tt = str(ft.title)
