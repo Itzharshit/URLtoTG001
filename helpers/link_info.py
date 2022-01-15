0d55bf659bfa4944f1d18bc03f42bdc9df8f93c9
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-01-15 19:18:31 +0530

    Update link_info.py

--- helpers/link_info.py
+++ helpers/link_info.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 async def linfo2(bot , m):
     
   if ("youtube.com" in m.text) or ("youtu.be" in m.text):
-    await m.reply_text(text=f"Youtube Link. Use /upload.", quote=True)
+    await m.reply_text(text=f"𝐘𝐨𝐮𝐓𝐮𝐛𝐞 𝐋𝐢𝐧𝐤. 𝐔𝐬𝐞 /upload.", quote=True)
     return
   
   if "|" in m.text:
@@ -28,7 +28,7 @@ async def linfo2(bot , m):
         fn = fn.split("\',", 1)[0]
     #logger.info(r.text)
     logger.info(fn)
-    await m.reply_text(text=f"📋 Link Info:\n\nFile: `{fn}`\n\nUse /upload.\n\nSee /help.", quote=True)
+    await m.reply_text(text=f"📋 𝐋𝐢𝐧𝐤 𝐢𝐧𝐟𝐨:\n\n𝐅𝐢𝐥𝐞: `{fn}`\n\n𝐔𝐬𝐞 /upload.\n\n𝐒𝐞𝐞 /help.", quote=True)
     return
   else:
     url = m.text.strip()
@@ -45,14 +45,14 @@ async def linfo2(bot , m):
             cfname = cfname.split("\"")[1]
           mt = mimetypes.guess_type(str(cfname))[0]
         else:
-          await m.reply_text(text=f"I Could not Determine The FileType !\nPlease Use Custom Filename With Extension\n\nSee /help.", quote=True)
+          await m.reply_text(text=f"𝐈 𝐜𝐨𝐮𝐥𝐝 𝐧𝐨𝐭 𝐝𝐞𝐭𝐞𝐫𝐦𝐢𝐧𝐞 𝐭𝐡𝐞 𝐟𝐢𝐥𝐞 𝐭𝐲𝐩𝐞 !\n𝐏𝐥𝐞𝐚𝐬𝐞 𝐮𝐬𝐞 𝐜𝐮𝐬𝐭𝐨𝐦 𝐟𝐢𝐥𝐞𝐧𝐚𝐦𝐞\n\n𝐒𝐞𝐞 /help.", quote=True)
           return
       except RequestException as e:
-        await m.reply_text(text=f"Error:\n\n{e}", quote=True)
+        await m.reply_text(text=f"𝐄𝐫𝐫𝐨𝐫:\n\n{e}", quote=True)
         return
         
   r = requests.get(url, allow_redirects=True, stream=True)
   url_size = int(r.headers.get("content-length", 0))
   url_size = get_size(url_size)
 
-  await m.reply_text(text=f"📋 Link Info:\n\nFile: `{cfname}`\nMime-Type: `{mt}`\nSize: `{url_size}`\n\nUse /upload as reply to your link, it will upload your link to telegram.\n\nSee /help.", quote=True)
+  await m.reply_text(text=f"📋 𝐋𝐢𝐧𝐤 𝐢𝐧𝐟𝐨:\n\n𝐅𝐢𝐥𝐞: `{cfname}`\nMime-Type: `{mt}`\n𝐒𝐢𝐳𝐞: `{url_size}`\n\n𝐔𝐬𝐞 /upload 𝐚𝐬 𝐫𝐞𝐩𝐥𝐲 𝐭𝐨 𝐲𝐨𝐮𝐫 𝐥𝐢𝐧𝐤, 𝐢𝐭 𝐰𝐢𝐥𝐥 𝐮𝐩𝐥𝐨𝐚𝐝 𝐲𝐨𝐮𝐫 𝐥𝐢𝐧𝐤 𝐭𝐨 𝐲𝐨𝐮𝐫 𝐭𝐞𝐥𝐞𝐠𝐫𝐚𝐦.\n\n𝐒𝐞𝐞 /help.", quote=True)
